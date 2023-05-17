601873a134af98ffffa621af0e75582abe0caa73
seif-maachaoui <seifeddine.maachaoui@laplateforme.io>
2023-05-17 17:37:40 +0200

    Modification du fichier index, et amélioration de quelques commentaires.

--- jour04/job02/form.php
+++ jour04/job02/form.php
@@ -2,14 +2,14 @@
 /* Déclaration d'un tableau qui contiendra les paramètres du formulaire */
 $elements = ['nom', 'prenom'];
 
-/* Je vérifie si le formulaire a été soumis via une requête POST
+/* Je vérifie si le formulaire a été soumis via une requête GET
 et si le bouton envoyer a été cliqué */
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btn'])){
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['btn'])){
     
       /* Je place une boucle pour vérifier si les champs du formulaire ont été remplis */
       foreach ($elements as $value) {
         //Si le champ à été rempli et qu'il n'est pas vide alors...
-        if (isset($_POST[$value]) && !empty($_POST[$value])) {
+        if (isset($_GET[$value]) && !empty($_GET[$value])) {
             echo "<p style='color:green'>Enregistrement réalisé avec succès !</p>";
             break;    
         }
@@ -31,16 +31,16 @@ echo "<thead>
 /* le corps du tableau, suivi de la ligne du tableau et des 
 cellules du tableau */
 
-// Lecture des données avec $POST au sein d'un tableau 
+// Récupération des données avec $GET au sein d'un tableau 
    echo "<tbody>";
     echo "<tr>
     <td>" .$elements[0] ."</td>
-    <td>" .$_POST['nom'] ."</td>
+    <td>" .$_GET['nom'] ."</td>
     </tr>
 
     <tr>
     <td>" .$elements[1] ."</td>
-    <td>" .$_POST['prenom'] ."</td>
+    <td>" .$_GET['prenom'] ."</td>
     </tr>";
 
 ?>
